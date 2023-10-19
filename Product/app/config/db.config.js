dcc38c80f55752ad4a6c764bf26e0c1ff4fe7c28
shiv291023 <saror139@my.centennialcollege.ca>
2023-10-18 22:06:17 -0400

    commit

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+
+module.exports = {
+
+  
+
+url: "mongodb+srv://arorashivkamar29:iloveyou3000@cluster0.oddvvai.mongodb.net/?retryWrites=true&w=majority"
+
+};
